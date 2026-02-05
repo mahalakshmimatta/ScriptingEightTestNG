1ec6911b5c2d273e730a0a2d61083a471dcb53ea
SG Tester <sitamahalakshmi4@gmail.com>
2026-02-05 21:28:22 +0530

    Modified classFiles And new class file and testng.xml file

--- ScriptingEight/src/testScripts/TestScript.java
+++ ScriptingEight/src/testScripts/TestScript.java
@@ -3,19 +3,22 @@ package testScripts;
 import methods.AppDependentMethods;
 import methods.AppIndependentMethods;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 import java.util.Map;
 
 public class TestScript {
-
+    @Test
     public static void TS_LoginAndLogout(){
         WebDriver oBrowser = null;
         Map<String, String> objData = null;
         try{
             objData = AppIndependentMethods.getExcelData("TestData1.xlsx", "testData", "TestId01");
             oBrowser = AppIndependentMethods.launchBrowser(objData.get("browserName"));
-            AppDependentMethods.navigateURL(oBrowser, objData.get("URL"));
-            AppDependentMethods.loginToApplication(oBrowser, objData.get("userName"), objData.get("password"));
-            AppDependentMethods.logoutFromApplication(oBrowser);
+            Assert.assertTrue(AppDependentMethods.navigateURL(oBrowser, objData.get("URL")), "Failed to load the URL");
+            Assert.assertTrue(AppDependentMethods.loginToApplication(oBrowser, objData.get("userName"), objData.get("password")), "Failed to login to actiTime");
+            Assert.assertTrue(AppDependentMethods.logoutFromApplication(oBrowser), "Failed to logout from the actiTime");
         }catch(Exception e){
             System.out.println("Exception in 'TS_LoginAndLogout()' test script. "+e);
         }finally{
@@ -24,18 +27,18 @@ public class TestScript {
         }
     }
 
-
+    @Test
     public static void TS_LoginCreateAndDeleteUserAndLogout(){
         WebDriver oBrowser = null;
         Map<String, String> objData = null;
         try{
             objData = AppIndependentMethods.getExcelData("TestData1.xlsx", "testData", "TestId02");
             oBrowser = AppIndependentMethods.launchBrowser(objData.get("browserName"));
-            AppDependentMethods.navigateURL(oBrowser, objData.get("URL"));
-            AppDependentMethods.loginToApplication(oBrowser, objData.get("userName"), objData.get("password"));
+            Assert.assertTrue(AppDependentMethods.navigateURL(oBrowser, objData.get("URL")),"Failed to load the URL");
+            Assert.assertTrue(AppDependentMethods.loginToApplication(oBrowser, objData.get("userName"), objData.get("password")),"Failed to login to actiTime");
             String userName = AppDependentMethods.createUser(oBrowser, objData);
-            AppDependentMethods.deleteUser(oBrowser, userName);
-            AppDependentMethods.logoutFromApplication(oBrowser);
+            Assert.assertTrue(AppDependentMethods.deleteUser(oBrowser, userName),"Failed to delete the user");
+            Assert.assertTrue(AppDependentMethods.logoutFromApplication(oBrowser),"Failed to logout from the actiTime");
         }catch(Exception e){
             System.out.println("Exception in 'TS_LoginAndLogout()' test script. "+e);
         }finally{
